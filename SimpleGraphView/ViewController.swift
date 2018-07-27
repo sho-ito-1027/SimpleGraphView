d42cc72dab2d31a5e770f8be9ae121577ac2bc1a
aryzae <a.aryzae@gmail.com>
2018-07-27 15:24:01 +0900

    初期値をセット

--- SimpleGraphView/ViewController.swift
+++ SimpleGraphView/ViewController.swift
@@ -16,21 +16,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var hGraph3Slider: UISlider!
     @IBOutlet weak var hMask1Slider: UISlider!
     // NSLayoutConstraint
-    @IBOutlet weak var hGraph1WidthConstraint: NSLayoutConstraint!
-    @IBOutlet weak var hGraph2WidthConstraint: NSLayoutConstraint!
-    @IBOutlet weak var hMaskWidthConstraint: NSLayoutConstraint!
+    @IBOutlet var hGraph1WidthConstraint: NSLayoutConstraint!
+    @IBOutlet var hGraph2WidthConstraint: NSLayoutConstraint!
+    @IBOutlet var hMaskWidthConstraint: NSLayoutConstraint!
     
     /// 縦グラフ
     // UISlider
     @IBOutlet weak var vGraph1Slider: UISlider!
     @IBOutlet weak var vGraph2Slider: UISlider!
     // NSLayoutConstraint
-    @IBOutlet weak var vGraph1HeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var vGraph2HeightConstraint: NSLayoutConstraint!
+    @IBOutlet var vGraph1HeightConstraint: NSLayoutConstraint!
+    @IBOutlet var vGraph2HeightConstraint: NSLayoutConstraint!
     
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        configureHorizontalSliderValue()
+        configureVerticalSliderValue()
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,6 +41,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func configureHorizontalSliderValue() {
+        // graph3に対するgraph1、graph2の割合を求めてセット
+        hGraph1WidthConstraint.changeMultiplier(to: hGraph1Slider.value / hGraph3Slider.value)
+        hGraph2WidthConstraint.changeMultiplier(to: hGraph2Slider.value / hGraph3Slider.value)
+        hMaskWidthConstraint.changeMultiplier(to: 1.0 - hMask1Slider.value)
+    }
+    
+    func configureVerticalSliderValue() {
+        vGraph1HeightConstraint.changeMultiplier(to: vGraph1Slider.value)
+        vGraph2HeightConstraint.changeMultiplier(to: vGraph2Slider.value)
+    }
 
 }
 
