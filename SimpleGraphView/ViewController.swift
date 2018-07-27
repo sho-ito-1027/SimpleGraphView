e60a34328198c5d97e8d255a9e44af25ff16047d
aryzae <a.aryzae@gmail.com>
2018-07-27 15:06:00 +0900

    Main画面のStoryboardとViewControllerの紐付け

--- SimpleGraphView/ViewController.swift
+++ SimpleGraphView/ViewController.swift
@@ -9,7 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    /// 横グラフ
+    // UISlider
+    @IBOutlet weak var hGraph1Slider: UISlider!
+    @IBOutlet weak var hGraph2Slider: UISlider!
+    @IBOutlet weak var hGraph3Slider: UISlider!
+    @IBOutlet weak var hMask1Slider: UISlider!
+    // NSLayoutConstraint
+    @IBOutlet weak var hGraph1WidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var hGraph2WidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var hMaskWidthConstraint: NSLayoutConstraint!
+    
+    /// 縦グラフ
+    // UISlider
+    @IBOutlet weak var vGraph1Slider: UISlider!
+    @IBOutlet weak var vGraph2Slider: UISlider!
+    // NSLayoutConstraint
+    @IBOutlet weak var vGraph1HeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var vGraph2HeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
